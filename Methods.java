18ae90da13716e1964b90f7b227d0337a0fe484c
natabe1la <147366746+natabe1la@users.noreply.github.com>
2023-12-04 13:17:22 +0800

    Update Methods.java

--- Methods.java
+++ Methods.java
@@ -1,29 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Bonilla_CPE
- */
 public class Methods {
     
     public static void main(String[]args){
   
         int factorial = FactorialofNumber(7);
         System.out.println(factorial);
-
+           
+        double Slope=Slope(4.3, 2.0, 3.1, 1.9);
+        System.out.println(Slope);
+        
+        PrinttheName();
 }
     
     static int FactorialofNumber(int expo){
    int ans=1;
     for(int i= 1; i <= expo; i++){
         ans=i*ans;
-    
+
     }
 return ans;
 }
+    
+    static double Slope( double y2, double y1, double x2, double x1){
+    return (y2-y1)/(x2-x1);   
+    }
+    
+    static void PrinttheName(){
+        for (int i = 0; i < 10; i++) {
+        System.out.println("Name");   
+        }
+    }
 }
+
 
